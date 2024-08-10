3bf2d6186601cf2449526ea202ce11a66bdcceec
nima.yaghouti <nima.yaghouti@partsoftware.com>
2024-08-11 01:10:36 +0330

    feat: add BaseButton component and define it as a global component

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import GlobalComponents from './plugins/global-components'
 // import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(GlobalComponents)
 // app.use(router)
 
 app.mount('#app')
