9fb65e7d2dc857109885641705e60abd37246090
nima.yaghouti <nima.yaghouti@partsoftware.com>
2024-08-12 19:22:14 +0330

    feat: login layout added and handled by vue router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
-import LoginView from './views/LoginView.vue'
+import { ref, watch, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { defineAsyncComponent } from 'vue'
+
+const route = useRoute()
+const layoutName = ref(route.meta.layout || 'LoginLayout')
+
+watch(
+  () => route.meta.layout,
+  (newLayout) => {
+    layoutName.value = newLayout || 'LoginLayout'
+  }
+)
+
+const currentLayout = computed(() =>
+  defineAsyncComponent(() => import(`@/layouts/${layoutName.value}.vue`))
+)
 </script>
 
 <template>
-  <!-- just for test -->
-  <LoginView />
+  <component :is="currentLayout" />
 </template>
 
 <style lang="scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue'),
+    meta: {
+      layout: 'LoginLayout'
+    }
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes
 })
 
 export default router
